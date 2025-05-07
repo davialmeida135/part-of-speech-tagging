b4ecc291c51bf5423ae0f837c21bb883ce585141
Davi Almeida <davi.almeida.135@ufrn.edu.br>
2025-05-07 19:40:47 -0300

    Cria coluna de total appearances

--- extractors/unigram.py
+++ extractors/unigram.py
@@ -4,12 +4,18 @@ from collections import Counter
 with open("data/raw/Secs0-18 - training", "r", encoding="utf-8") as f:
     content = f.read()
 
+# Separa todos os tokens
 tokens = content.strip().split()
 pairs = []
 for token in tokens:
     if "_" not in token:
         continue
     word, tag = token.rsplit("_", 1)
+    try:
+        word = float(word)
+        word = "numeric-word"
+    except:
+        pass
     pairs.append((word, tag))
 
 counts = Counter(pairs)
@@ -22,5 +28,17 @@ df = pl.DataFrame([
 df = df.sort(["count"], descending=True)
 df = df.pivot(on="tag",index="word",values="count")
 
-print(df)
+# Cria coluna de contagem total
+df = df.with_columns(
+    appearances = pl.sum_horizontal(col for col in df.columns[1:])
+)
+
+# Transforma todas palavras com count=1 em unk-word
+df = df.with_columns(
+
+)
+
+# Seleciona apenas a coluna com maior count
+
+
 df.to_pandas().to_csv("data/dataset/unigram_pl.csv")
